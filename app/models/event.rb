155017c9c3e2ddeb35794fdf50889baa7e6463e3
Will Robinson <will@wambl.com>
2015-05-31 13:06:59 -0700

    Show Page

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,6 @@ class Event < ActiveRecord::Base
 	
 	has_many :rounds
 
-	validates_presence_of :user_id,:course_id,:title,:description,:start_date,:end_date
+	validates_presence_of :user_id,:course_id,:title,:description,:start_date
 
 end
